8059ccfb0d28b23d6236bbc69e68b061eee2739c
Carson Michael Thrift <thrift.carson6@gmail.com>
2017-06-05 14:36:06 -0400

    Added the ability to change headers and the colors of both the text and the background of the page

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 "# People Factory" 
+
+## Day 1 Homework
+
+*Add another input to the form. (x)
+*use the values from both inputs in the 'h1'.(x) 
+
+### Bonus
+
+*Add an empty paragraph to the page.(x)
+*Use the form values to do something with that paragraph.(x)
+
+###Bonus Bonus
+
+*Change the appearance of the paragraph (think CSS) based on a value from the form.(x)
+
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const personForm = document.querySelector('form#personForm')
+// const submitBtn = document.querySelector('#submitButton')
+// const italicsBtn = document.querySelector('italicsBtn')
+// const boldBtn = document.querySelector('boldBtn')
+// const underBtn = document.querySelector('underBtn')
+
 
 function handleSubmit(ev){
     ev.preventDefault()
     const f = ev.target
-    console.log(f.personName.value)
     const name = f.personName.value
-    document.querySelector('h1').textContent = name
+    const bday = f.personBday.value
+    const textColor = f.textColor.value
+    const backColor = f.backColor.value
 
+    document.querySelector('h1').textContent = name
+    document.querySelector('h2').textContent = bday
+    document.querySelector('#P1').style.color = textColor
+    document.bgColor = backColor
 }
 
+// function handleItalic(ev){
+//     ev.preventDefault()
+//     document.querySelector('#P1').style.font = "italic"
+// }
+
+// function handleBold(ev){
+//     ev.preventDefault()
+//     document.querySelector('#P1').style.font = "bold"
+// }
+
+// function handleUnderline(ev){
+//     ev.preventDefault()
+//     document.querySelector('#P1').style.font = "underline"
+// }
+
 personForm.addEventListener('submit', handleSubmit)
+// italicsBtn.addEventListener('submit', handleItalic)
+// boldBtn.addEventListener('submit', handleBold)
+// underBtn.addEventListener('submit', handleUnderline)
+
+
